02b0e269af0d619ffc86858326ac82f04d288d85
vasony <vasony.tamas@gmail.com>
2021-03-21 16:46:50 +0100

    data: update nav-adoslita to 20201231

--- nav-adoslista/1get.bat
+++ nav-adoslista/1get.bat
@@ -1,4 +1,4 @@
 call 0config.bat
-wget -N https://www.nav.gov.hu/data/cms160317/180nap_MSZem_%DATUM%.pdf
-wget -N https://www.nav.gov.hu/data/cms160318/180NAP_GAZD_%DATUM%.pdf
-wget -N https://www.nav.gov.hu/data/cms533097/%NEV%._ne_web.xls
+wget -N https://www.nav.gov.hu/data/cms160317/180nap_MSZem_%DATUM%WEB.pdf
+wget -N https://www.nav.gov.hu/data/cms160318/180NAP_GAZD_%DATUM%WEB.pdf
+wget -N https://www.nav.gov.hu/data/cms540445/%NEV%._ne_web.xls
